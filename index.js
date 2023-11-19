aaedb32e6308d23800d21983099167d8b1fab78f
BayuM26 <bayu.m884@gmail.com>
2023-11-19 08:31:07 +0700

    update kerapihan

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express')
-const routes = express.Router()
 const app = express()
 app.use(express.json())
+
 // call controller 
-// const ControllerSertifikat = require("./Controller/ControllerSertifikat")
-// const ControllerPortfolio = require("./Controller/Con
-// const ControllerAbaoutMe = require("./Controller/ControllerAboutMe")
 const ControllerSertifikat = require('./Controller/SertifikatController')
 
+// body parser
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // routing 
+const routes = express.Router()
+
 routes.route('/sertifikat')
       .get(ControllerSertifikat.ShowSertifikat)
       .post(ControllerSertifikat.CreateSertifikat)
@@ -21,32 +21,14 @@ routes.route('/sertifikat/:id')
       .patch(ControllerSertifikat.UpdateSertifikat)
       .delete(ControllerSertifikat.DeleteSertifikat);
 
-// routes.route('/portfolio')
-//       .get(ControllerPortfolio.Portfolio)
-//       .post(ControllerPortfolio.CreatePortfolio);
-// routes.route('/portfolio/:id')
-//       .patch(ControllerPortfolio.UpdatePortfolio)
-//       .delete(ControllerPortfolio.DeletePortfolio);
-
-// routes.route('/aboutMe')
-//       .get(ControllerAbaoutMe.AboutMe)
-//       .post(ControllerAbaoutMe.CreateAboutMe);
-// routes.route('/aboutMe/:id')
-//       .patch(ControllerAbaoutMe.UpdateAboutMe)
-//       .delete(ControllerAbaoutMe.DeleteAboutMe);
-
 app.use(routes)
 
-
-// port setting 
+// dotenv
 const dotenv = require('dotenv')
-const db = require('./connection')
-// const bodyParser = require('body-parser')
-
 dotenv.config()
 
+// port setting 
 const port = process.env.PORT
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
